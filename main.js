ae3f658c82c5f2b45cb22d9847d88b6a060c8379
ishitadudhat <ishitadudhat2501@gmail.com>
2024-05-22 16:34:07 +0530

    Add JS file and button

---
+++ main.js
@@ -0,0 +1,11 @@
+function darkbtn(){
+    var a= document.body.style;
+    a.background="black";
+    a.color="white";
+}
+function lightbtn(){
+    var b= document.body.style;
+    b.background="white";
+    b.color="black";
+}
+
